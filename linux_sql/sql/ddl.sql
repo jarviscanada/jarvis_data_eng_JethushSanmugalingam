45a2b4667737a1457914e7bc32dd220bb83f9243
Jethush Sanmugalingam <jethush66@gmail.com>
2026-02-24 16:13:55 +0000

    remove sample data

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -1,3 +1,5 @@
+/c host_agent;
+
 CREATE TABLE IF NOT EXISTS public.host_info
 (
     id               SERIAL NOT NULL,
@@ -13,22 +15,6 @@ CREATE TABLE IF NOT EXISTS public.host_info
     CONSTRAINT host_info_un UNIQUE (hostname)
 );
 
-INSERT INTO host_info
-(hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, "timestamp")
-VALUES
-('jrvs-remote-desktop-centos7-6.us-central1-a.c.spry-framework-236416.internal',
- 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, 601324, '2019-05-29 17:49:53')
-ON CONFLICT (hostname) DO NOTHING;
-
-INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, "timestamp")
-VALUES
-('noe1', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, 601324, '2019-05-29 17:49:53')
-ON CONFLICT (hostname) DO NOTHING;
-
-INSERT INTO host_info (hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, "timestamp")
-VALUES
-('noe2', 1, 'x86_64', 'Intel(R) Xeon(R) CPU @ 2.30GHz', 2300, 256, 601324, '2019-05-29 17:49:53')
-ON CONFLICT (hostname) DO NOTHING;
 
 CREATE TABLE IF NOT EXISTS public.host_usage
 (
@@ -41,14 +27,3 @@ CREATE TABLE IF NOT EXISTS public.host_usage
     disk_available INT4 NOT NULL,
     CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES host_info(id)
 );
-
-INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-VALUES
-('2019-05-29 15:00:00', 1, 300000, 90, 4, 2, 3)
-ON CONFLICT DO NOTHING;
-
-INSERT INTO host_usage ("timestamp", host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-VALUES
-('2019-05-29 15:01:00', 1, 200000, 90, 4, 2, 3)
-ON CONFLICT DO NOTHING;
-
